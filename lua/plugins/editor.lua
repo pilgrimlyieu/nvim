53cb21550744fb000e8753e32a492ab723b6d294
PilgrimLyieu <pilgrimlyieu@outlook.com>
2026-07-04 20:11:18 +0800

    feat(zh): 添加 flash-zh.nvim 支持

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -5,4 +5,26 @@ return {
       { "S", mode = { "n", "o", "x" }, false },
     },
   },
+  {
+    "rainzm/flash-zh.nvim",
+    dependencies = "folke/flash.nvim",
+    keys = {
+      {
+        "gz",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash-zh").jump({ chinese_only = false })
+        end,
+        desc = "Flash Chinese",
+      },
+      {
+        "gZ",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash-zh").jump({ chinese_only = true })
+        end,
+        desc = "Flash Chinese Only",
+      },
+    },
+  },
 }
