0241d4c891b9fe34b08e92ac7db7f4833d38111e
PilgrimLyieu <pilgrimlyieu@outlook.com>
2026-08-09 14:47:38 +0800

    fix: 修复 Windows PowerShell 相关 shell 配置问题

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -21,8 +21,8 @@ if vim.fn.has("win32") == 1 then
   opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
   opt.shellcmdflag =
     "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
-  opt.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-  opt.shellpipe = '2>&1 | %{ "$_" } | tee %s; exit $LastExitCode'
+  opt.shellredir = '2>&1 | ForEach-Object { "$_" } | Out-File %s; exit $LastExitCode'
+  opt.shellpipe = '2>&1 | ForEach-Object { "$_" } | tee %s; exit $LastExitCode'
   opt.shellquote = ""
   opt.shellxquote = ""
 end
